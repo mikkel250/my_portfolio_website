2c4ae18867b8aabae72f5534e1638678b4216fea
Mikkel Ridley <mikkel.ridley@kulcbd.com>
2022-12-05 13:10:51 -0800

    set up deploy to netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import sveltePreprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
 import { mdsvex } from 'mdsvex'; 
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		target: '#svelte'
 	},
 	extensions: ['.svelte', '.md' ],
 	preprocess: [
